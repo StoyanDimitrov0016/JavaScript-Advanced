c30d46b32d522bad2a72046a0a61ab3dd5ef6c68
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-03-08 15:46:51 +0200

    Uploading solved tasks

--- JS-Advanced/Syntax/largestNumber.js
+++ JS-Advanced/Syntax/largestNumber.js
@@ -2,4 +2,4 @@ function largestNum(num1, num2, num3) {
     const largest = Math.max(num1, num2, num3);
     console.log(`The largest number is ${largest}.`);
 }
-largestNum(5, -2, 16)
+largestNum(5, -2, 16);
