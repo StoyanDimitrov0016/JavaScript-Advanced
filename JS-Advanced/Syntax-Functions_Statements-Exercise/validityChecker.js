11d41437a72d4770d8a072e5ab629437dd1df77e
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-03-09 16:41:53 +0200

    Update validityChecker.js

--- JS-Advanced/Syntax-Functions_Statements-Exercise/validityChecker.js
+++ JS-Advanced/Syntax-Functions_Statements-Exercise/validityChecker.js
@@ -1,18 +1,23 @@
-function solve(x1, y1, x2, y2) {
+function validOrNotDistance(x1, y1, x2, y2) {
     const distanceOnesToZero = Math.sqrt(x1 ** 2 + y1 ** 2);
     const distanceTwosToZero = Math.sqrt(x2 ** 2 + y2 ** 2);
     const distanceToEachOther = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
-    if (distanceOnesToZero % 1 == 0) {
+
+    //A distance between two points in Cartesian coordinate system (0, 0) is considered valid if it is an integer value
+
+    if (distanceOnesToZero % 1 === 0) {
         console.log(`{${x1}, ${y1}} to {0, 0} is valid`);
     } else {
         console.log(`{${x1}, ${y1}} to {0, 0} is invalid`);
     }
-    if (distanceTwosToZero % 1 == 0) {
+
+    if (distanceTwosToZero % 1 === 0) {
         console.log(`{${x2}, ${y2}} to {0, 0} is valid`);
     } else {
         console.log(`{${x2}, ${y2}} to {0, 0} is invalid`);
     }
-    if (distanceToEachOther % 1 == 0) {
+
+    if (distanceToEachOther % 1 === 0) {
         console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is valid`);
     } else {
         console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is invalid`);
